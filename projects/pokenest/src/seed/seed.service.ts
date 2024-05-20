731030e946742910a0ef9b93864e4d52079d1ebf
alexander <castro.t.alex@gmail.com>
2024-05-19 19:37:54 -0500

    adding the axios pack and get seed from pokeapi

--- projects/pokenest/src/seed/seed.service.ts
+++ projects/pokenest/src/seed/seed.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
-
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-interface';
 @Injectable()
 export class SeedService {
+  private readonly axios: AxiosInstance = axios;
+
   async executeSeed() {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=151').then((response) => {
-      console.log('response', response);
-      return response.json();
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=2',
+    );
+
+    data.results.forEach((pokemon) => {
+      const { name, url } = pokemon;
+
+      const segments = url.split('/');
+      const no = segments[segments.length - 2];
+
+      console.log(`Pokemon: ${name} - No: ${no}`);
     });
+
+    return data;
   }
 }
